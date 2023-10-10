9c1357cb589b6ae7f68fc799faf384a91e3c8ae2
bcanevaro <bcanevaro@itba.edu.ar>
2023-10-10 17:41:41 -0300

    Added some endpoints to the api.

--- api/routes/e01_cliente.js
+++ api/routes/e01_cliente.js
@@ -2,11 +2,70 @@ const { Router, response } = require("express");
 const pool = require("../database");
 const router = Router();
 
-router.get("/", (request, response) => {
-    pool.query("SELECT * FROM e01_cliente", (err, res) => {
-        if (err) response.end(err.message);
-        response.json(res.rows);
+// POST route to insert data
+router.post("/insert", (request, response) => {
+    const { nro_cliente, nombre, apellido, direccion, activo } = request.body; // Assuming you have a JSON request body with name and email fields
+
+    if (!nro_cliente || !nombre || !apellido || !direccion || !activo) {
+        response.status(400).json({ error: "Todos los parámetros son requeridos para insertar el cliente en la tabla." });
+        return;
+    }
+
+    // SQL query to insert data into the e01_cliente table
+    const query = "INSERT INTO e01_cliente (nro_cliente,nombre,apellido,direccion,activo) VALUES ($1, $2, $3, $4, $5)";
+    const values = [nro_cliente,nombre,apellido,direccion,activo];
+
+    pool.query(query, values, (err, res) => {
+        if (err) {
+            response.status(500).json({ error: err.message });
+        } else {
+            response.status(201).json({ message: "Cliente agregado correctamente." });
+        }
     });
 });
+
+// POST route to insert data
+router.delete("/delete", (request, response) => {
+    const { nro_cliente } = request.body; // Assuming you have a JSON request body with name and email fields
+
+    if (!nro_cliente) {
+        response.status(400).json({ error: "nro_cliente es requerido para borrar al cliente de la tabla." });
+        return;
+    }
+
+    // SQL query to insert data into the e01_cliente table
+    const query = "DELETE FROM e01_cliente WHERE nro_cliente = ($1)";
+    const values = [nro_cliente];
+
+    pool.query(query, values, (err, res) => {
+        if (err) {
+            response.status(500).json({ error: err.message });
+        } else {
+            response.status(201).json({ message: "Cliente eliminado correctamente." });
+        }
+    });
+});
+
+router.put("/put", (request, response) => {
+    const { nro_cliente, nombre, apellido, direccion, activo } = request.body;  // Assuming you have a JSON request body with name and email fields
+
+    if (!nro_cliente) {
+        response.status(400).json({ error: "nro_cliente es requerido para actualizar al cliente de la tabla." });
+        return;
+    }
+
+    // SQL query to insert data into the e01_cliente table
+    const query = "UPDATE e01_cliente SET nombre = $2, apellido = $3 , direccion = $4, activo = $5 WHERE nro_cliente = ($1)";
+    const values = [nro_cliente,nombre,apellido,direccion,activo];
+
+    pool.query(query, values, (err, res) => {
+        if (err) {
+            response.status(500).json({ error: err.message });
+        } else {
+            response.status(201).json({ message: "Cliente actualizado correctamente." });
+        }
+    });
+});
+
 
 module.exports = router;
--- api/routes/e01_producto.js
+++ api/routes/e01_producto.js
@@ -2,10 +2,46 @@ const { Router, response } = require("express");
 const pool = require("../database");
 const router = Router();
 
-router.get("/", (request, response) => {
-    pool.query("SELECT * FROM e01_producto", (err, res) => {
-        if (err) response.end(err.message);
-        response.json(res.rows);
+// POST route to insert data
+router.post("/insert", (request, response) => {
+    const { codigo_producto, marca, nombre, descripcion, precio, stock } = request.body; // Assuming you have a JSON request body with name and email fields
+
+    if (!codigo_producto || !marca || !nombre || !descripcion || !precio || !stock) {
+        response.status(400).json({ error: "Todos los parámetros son requeridos para insertar el producto en la tabla." });
+        return;
+    }
+
+    // SQL query to insert data into the e01_prodcuto table
+    const query = "INSERT INTO e01_producto (codigo_producto, marca, nombre, descripcion, precio, stock) VALUES ($1, $2, $3, $4, $5, $6)";
+    const values = [codigo_producto, marca, nombre, descripcion, precio, stock];
+
+    pool.query(query, values, (err, res) => {
+        if (err) {
+            response.status(500).json({ error: err.message });
+        } else {
+            response.status(201).json({ message: "Producto agregado correctamente." });
+        }
+    });
+});
+
+router.put("/put", (request, response) => {
+    const { codigo_producto, marca, nombre, descripcion, precio, stock } = request.body;  // Assuming you have a JSON request body with name and email fields
+
+    if (!codigo_producto) {
+        response.status(400).json({ error: "codigo_producto es requerido para actualizar el prodcuto de la tabla." });
+        return;
+    }
+
+    // SQL query to insert data into the e01_cliente table
+    const query = "UPDATE e01_producto SET marca = $2, nombre = $3 , descripcion = $4, precio = $5, stock = $6 WHERE codigo_producto = ($1)";
+    const values = [codigo_producto, marca, nombre, descripcion, precio, stock ];
+
+    pool.query(query, values, (err, res) => {
+        if (err) {
+            response.status(500).json({ error: err.message });
+        } else {
+            response.status(201).json({ message: "Producto actualizado correctamente." });
+        }
     });
 });
 
